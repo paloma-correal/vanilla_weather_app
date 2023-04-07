5326022fbae2c10699a2872e6e482aca296a337b
Affectionate-kitten <127782469+paloma-correal@users.noreply.github.com>
2023-04-07 18:50:55 -0400

    Edited API URL on HTML and JS and changed IDs for unit conversions

--- scr/app.js
+++ scr/app.js
@@ -1,11 +1,11 @@
-function formatDate(timestamp){
+function formatDate(timestamp) {
     let date = new Date(timestamp);
     let hours = date.getHours();
-    if (hours < 10){
+    if (hours < 10) {
         hours = `0${hours}`;
     }
     let minutes = date.getMinutes();
-    if (minutes < 10){
+    if (minutes < 10) {
         minutes = `0&{minutes}`;
     }
     let days = [
@@ -18,65 +18,67 @@ function formatDate(timestamp){
         "Saturday"
     ];
 
-let day = days[date.getDay()];
-return `${day} ${hours}:${minutes}`;
+    let day = days[date.getDay()];
+    return `${day} ${hours}:${minutes}`;
 }
 
-function displayTemperature(response){
-    console.log (response.data);
-    let temperatureElement = document.querySelector("Temperature");
-    let cityElement = document.querySelector ("City");
-    let descriptionElement = document.querySelector ("Description");
-    let humidityElement = document.querySelector ("Humidity");
-    let windElement = document.querySelector ("Wind");
-    temperatureElement.innerHTML = Math.round (response.data.main.temp);
-    cityElement.innerHTML = response.data.name;
-    descriptionElement.innerHTML = response.data.weather[0].description;
-    humidityElement.innerHTML = response.data.main.humidity;
-    windElement.innerHTML = Math.round (response.data.wind.speed);
-    dateElement.innerHTML = formatDate(response.data.dt * 1000);
+function displayTemperature(response) {
+    console.log(response.data);
+    let temperatureElement = document.querySelector("#temperature");
+    let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
+    let dateElement = document.querySelector("#date");
+    let iconElement = document.querySelector("#icon");
+    temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+    cityElement.innerHTML = response.data.city;
+    descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = response.data.temperature.humidity;
+    windElement.innerHTML = Math.round(response.data.wind.speed);
+    dateElement.innerHTML = formatDate(response.data.time * 1000);
     iconElement.setAttribute(
         "scr",
-        `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.weather[0].icon}.png`
+        `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
     );
-    iconElement.setAttribute("alt", response.data.weather[0].description);
+    iconElement.setAttribute("alt", response.data.condition.description);
 }
 
-    function search(city){
+function search(city) {
     let apiKey = "4t499e5cbb23dd05b04o16b8befa8cff";
-    let apiUrl = "http://api.shecodes.io/weather/v1/current?query=New York&key=4t499e5cbb23dd05b04o16b8befa8cff&units=metric";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=4t499e5cbb23dd05b04o16b8befa8cff&units=metric`;
     axios.get(apiUrl).then(displayTemperature);
-    }
+}
 
-    function handleSubmit(event){
-        event.preventDefault();
-        let cityInputElement = document.querySelector("city-input");
-        search(cityInputElement.value);
-    }
+function handleSubmit(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+}
 
-    function displayFahrenheitTemperature (event){
-        event.preventDefault();
-        let fahrenheitTemperature = (14 * 9)/5 +32;
-    
-        let temperatureElement = document.querySelector ("#temperature");
-        temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
-    }
+function displayFahrenheitTemperature(event) {
+    event.preventDefault();
+    let fahrenheitTemperature = (14 * 9) / 5 + 32;
 
-    function displayCelsiusTemperature (event){
-        event.preventDefault();
-        let temperatureElement = document.querySelector ("#temperature");
-        temperatureElement.innerHTML = Math.round(celsiusTemperature);
-    }
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
 
-    let celsiusTemperature = null;
+function displayCelsiusTemperature(event) {
+    event.preventDefault();
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
 
-    let form = document.querySelector ("search-form");
-    form.addEventListener("submit", handleSubmit);
+let celsiusTemperature = null;
 
-    let fahrenheitLink = document.querySelector ("#fahrenheit-link");
-    fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
 
-    let celsiusTemperature = document.querySelector ("celsius-link");
-    celsiusLink.addEventListener ("click", displayCelsiusTemperature);
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
-    search("New York");
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+search("New York");
