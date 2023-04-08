8c91cc3fd2e320820a6b48d83ccf435f893781b7
Affectionate-kitten <127782469+paloma-correal@users.noreply.github.com>
2023-04-07 20:12:34 -0400

    Created forecast function and string for days on .js

--- scr/app.js
+++ scr/app.js
@@ -22,6 +22,40 @@ function formatDate(timestamp) {
     return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast(){
+    let forecastElement = document.querySelector ("#forecast");
+
+    let forecastHTML = `<div class = "row">`;
+    let days = ["Thu", "Fri", "Sat", "Sun"];
+    days.forEach(function (day) {
+    forecastHTML = 
+    forecastHTML + `
+            
+             <div class = "col-2">
+               <div class = "weather-forecast-date">${day}</div>
+               <img
+               src = "http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-night.png"
+               alt = ""
+               width = "46"
+            />
+            <div class = "weather-forecast-temperatures">
+              <span class = "weather-forecast-temperature-max">
+                18°</span>
+              <span class = "weather-forecast-temperature-min">
+                12°</span>
+            </div>
+          </div>
+        
+        `;
+    });
+    
+    forecastHTML = forecastHTML + `</div>`
+    forecastElement.innerHTML = forecastHTML;
+    
+}
+
+
+
 function displayTemperature(response) {
     console.log(response.data);
     let temperatureElement = document.querySelector("#temperature");
@@ -82,3 +116,5 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("New York");
+
+displayForecast();
